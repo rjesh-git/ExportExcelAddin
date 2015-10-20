6766b95a05d28adb9e44658312e7427b21880c72
rjesh <rajesh>
2015-10-20 10:17:48 -0700

    Introduced adalHelper module and seperated the ADAL processes

---
+++ adalHelper.js
@@ -0,0 +1,82 @@
+var logger = require('connect-logger');
+var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
+var fs = require('fs');
+var crypto = require('crypto');
+var AuthenticationContext = require('adal-node').AuthenticationContext;
+
+process.env['parameters'] = 'parameters.json';
+
+var parametersFile = process.argv[2] || process.env['parameters'];
+
+var sampleParameters;
+if (parametersFile) {
+  var jsonFile = fs.readFileSync(parametersFile);
+  if (jsonFile) {
+    sampleParameters = JSON.parse(jsonFile);
+  } else {
+    console.log('File not found, falling back to defaults: ' + parametersFile);
+  }
+}
+
+if (!parametersFile) {
+  sampleParameters = {
+    tenant : 'REPLACETHIS.onmicrosoft.com',
+    authorityHostUrl : 'https://login.windows.net',
+    clientId : '89bedb2a-3c1e-4df6-b544-8f1f14392ebd',
+    clientSecret : 'vM2XycJD8lf29qfckwGC604ATqUBYTFcIsxvdnZuNFo='
+  };
+}
+
+var authorityUrl = sampleParameters.authorityHostUrl + '/' + sampleParameters.tenant;
+var redirectUri = 'http://localhost:3000/getAToken';
+var resource = '00000002-0000-0000-c000-000000000000';
+
+
+var templateAuthzUrl = 'https://login.windows.net/' + sampleParameters.tenant + '/oauth2/authorize?response_type=code&client_id=<client_id>&redirect_uri=<redirect_uri>&state=<state>&resource=<resource>';
+
+function createAuthorizationUrl(state) {
+  var authorizationUrl = templateAuthzUrl.replace('<client_id>', sampleParameters.clientId);
+  authorizationUrl = authorizationUrl.replace('<redirect_uri>',redirectUri);
+  authorizationUrl = authorizationUrl.replace('<state>', state);
+  authorizationUrl = authorizationUrl.replace('<resource>', resource);
+  return authorizationUrl;
+}
+
+exports.processGetToken = function (req, res) {
+  if (req.cookies.authstate !== req.query.state) {
+    res.send('error: state does not match');
+  }
+  var authenticationContext = new AuthenticationContext(authorityUrl);
+  authenticationContext.acquireTokenWithAuthorizationCode(req.query.code, redirectUri, resource, sampleParameters.clientId, sampleParameters.clientSecret, function (err, response) {
+    var message = '';
+    if (err) {
+      message = 'error: ' + err.message + '\n';
+    }
+    message += 'response: ' + JSON.stringify(response);
+
+    if (err) {
+      res.redirect('report');
+      return;
+    }
+
+    // Later, if the access token is expired it can be refreshed.
+    authenticationContext.acquireTokenWithRefreshToken(response.refreshToken, sampleParameters.clientId, sampleParameters.clientSecret, resource, function (refreshErr, refreshResponse) {
+      if (refreshErr) {
+        message += 'refreshError: ' + refreshErr.message + '\n';
+      }
+      message += 'refreshResponse: ' + JSON.stringify(refreshResponse);
+      res.redirect('report');
+    });
+  });
+};
+
+exports.processAuth = function(req, res) {
+
+  crypto.randomBytes(48, function(ex, buf) {
+    var token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
+    res.cookie('authstate', token);
+    var authorizationUrl = createAuthorizationUrl(token);
+    res.redirect(authorizationUrl);
+  });
+};
--- main.js
+++ main.js
@@ -4,10 +4,9 @@ var express = require('express');
 var logger = require('connect-logger');
 var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
-var fs = require('fs');
-var crypto = require('crypto');
 
 var AuthenticationContext = require('adal-node').AuthenticationContext;
+var adalHelper = require('./adalHelper.js');
 
 var viewID ='', 
     listID='';
@@ -20,81 +19,15 @@ app.use(session({secret: '1234567890QWERTY'}));
 app.get('/report', function(req, res) {
   res.send('TODO:Excel will be sent here list: '+ listID +', view:' +viewID);
 });
-process.env['parameters'] = 'parameters.json';
-
-var parametersFile = process.argv[2] || process.env['parameters'];
-
-var sampleParameters;
-if (parametersFile) {
-  var jsonFile = fs.readFileSync(parametersFile);
-  if (jsonFile) {
-    sampleParameters = JSON.parse(jsonFile);
-  } else {
-    console.log('File not found, falling back to defaults: ' + parametersFile);
-  }
-}
-
-if (!parametersFile) {
-  sampleParameters = {
-    tenant : 'REPLACETHIS.onmicrosoft.com',
-    authorityHostUrl : 'https://login.windows.net',
-    clientId : '89bedb2a-3c1e-4df6-b544-8f1f14392ebd',
-    clientSecret : 'vM2XycJD8lf29qfckwGC604ATqUBYTFcIsxvdnZuNFo='
-  };
-}
-
-var authorityUrl = sampleParameters.authorityHostUrl + '/' + sampleParameters.tenant;
-var redirectUri = 'http://localhost:3000/getAToken';
-var resource = '00000002-0000-0000-c000-000000000000';
-
-var templateAuthzUrl = 'https://login.windows.net/' + sampleParameters.tenant + '/oauth2/authorize?response_type=code&client_id=<client_id>&redirect_uri=<redirect_uri>&state=<state>&resource=<resource>';
-
-function createAuthorizationUrl(state) {
-  var authorizationUrl = templateAuthzUrl.replace('<client_id>', sampleParameters.clientId);
-  authorizationUrl = authorizationUrl.replace('<redirect_uri>',redirectUri);
-  authorizationUrl = authorizationUrl.replace('<state>', state);
-  authorizationUrl = authorizationUrl.replace('<resource>', resource);
-  return authorizationUrl;
-}
 
 app.get('/export', function(req, res) {
   viewID = req.query.viewID;
   listID = req.query.listID;
-  
-  crypto.randomBytes(48, function(ex, buf) {
-    var token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
-    res.cookie('authstate', token);
-    var authorizationUrl = createAuthorizationUrl(token);
-    res.redirect(authorizationUrl);
-  });
+  adalHelper.processAuth(req, res);
 });
 
 app.get('/getAToken', function (req, res) {
-  if (req.cookies.authstate !== req.query.state) {
-    res.send('error: state does not match');
-  }
-  var authenticationContext = new AuthenticationContext(authorityUrl);
-  authenticationContext.acquireTokenWithAuthorizationCode(req.query.code, redirectUri, resource, sampleParameters.clientId, sampleParameters.clientSecret, function (err, response) {
-    var message = '';
-    if (err) {
-      message = 'error: ' + err.message + '\n';
-    }
-    message += 'response: ' + JSON.stringify(response);
-
-    if (err) {
-      res.redirect('report');
-      return;
-    }
-
-    // Later, if the access token is expired it can be refreshed.
-    authenticationContext.acquireTokenWithRefreshToken(response.refreshToken, sampleParameters.clientId, sampleParameters.clientSecret, resource, function (refreshErr, refreshResponse) {
-      if (refreshErr) {
-        message += 'refreshError: ' + refreshErr.message + '\n';
-      }
-      message += 'refreshResponse: ' + JSON.stringify(refreshResponse);
-      res.redirect('report');
-    });
-  });
+  adalHelper.processGetToken(req, res);
 });
 
 app.listen(3000);
